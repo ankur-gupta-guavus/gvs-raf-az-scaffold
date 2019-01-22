c85c532d9ddaf817fce75004806d8cde2670f8be
Ankur Gupta <ankur.gupta@guavus.com>
2019-01-22 18:20:20 +0100

    Update Cdap-install_v1.sh

--- raf-hdi-cdap/scripts/Cdap-install_v1.sh
+++ raf-hdi-cdap/scripts/Cdap-install_v1.sh
@@ -3,7 +3,7 @@
 
 die() { echo "ERROR: ${*}"; exit 1; };
 
-for i in `/etc/init.d/cdap-* | awk -F'/' '{print $NF}'`
+for i in `ls -1 /etc/init.d/cdap-* | awk -F'/' '{print $NF}'`
 do
   systemctl enable ${i} && systemctl start ${i} || die "Failed to start ${i}"
 done
